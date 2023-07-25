6e4fc753993806efdc5e4d2146e149f42bc885a2
Gekon <kuba@titandesign.pl>
2023-07-25 18:41:28 +0200

    [CC-4]: added unit test for board.component.spec.ts

--- apps/cc/src/app/components/board/board.component.spec.ts
+++ apps/cc/src/app/components/board/board.component.spec.ts
@@ -1,5 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BoardComponent } from './board.component';
+import { BoardService } from './board.service';
+import { DashboardItem } from '../../models';
+import { DefaultDashboardConfig } from '../../config';
+import { BehaviorSubject } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { GridsterItemComponent, GridsterModule } from 'angular-gridster2';
+import { CommonModule } from '@angular/common';
+import { WebViewComponent } from './board-items/web-view/web-view.component';
+import { SafePipe } from '../../pipes/safe.pipe';
 
 describe('BoardComponent', () => {
   let component: BoardComponent;
@@ -7,7 +16,15 @@ describe('BoardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BoardComponent],
+      declarations: [BoardComponent, WebViewComponent, SafePipe],
+      imports: [CommonModule, GridsterModule],
+      providers: [
+        {
+          provide: BoardService, useValue: {
+            boardItems$: new BehaviorSubject<DashboardItem[]>(DefaultDashboardConfig)
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(BoardComponent);
@@ -18,4 +35,11 @@ describe('BoardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should renderBoardItems', () => {
+    const items = fixture.debugElement.queryAll(By.directive(GridsterItemComponent));
+    expect(items.length).toEqual(DefaultDashboardConfig.length);
+  });
+
+
 });
